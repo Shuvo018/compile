7ab4282d50cc487757f5bb7b41dfcb3762d7389b
Shuvo dev nath <80082187+Shuvo018@users.noreply.github.com>
2022-06-27 10:46:14 -0700

    Add files via upload

---
+++ temp/iu0njml.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {    
+
+    int number1=5, number2=5, sum;
+    
+    
+
+    // calculating sum
+    sum = number1 + number2;      
+    
+    printf("%d + %d = %d", number1, number2, sum);
+    return 0;
+}
